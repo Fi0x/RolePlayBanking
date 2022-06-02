528666280490d81ed444eb0777c1ace03992ceba
Fi0x <felpech@aol.com>
2022-06-02 13:05:09 +0200

    Added placeholder for all required activities

--- app/src/main/java/com/example/roleplaybanking/AccountSelectionFragment.java
+++ app/src/main/java/com/example/roleplaybanking/AccountSelectionFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class AccountSelectionFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,7 +17,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_account_selection, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -26,7 +26,7 @@ public class FirstFragment extends Fragment {
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
+                NavHostFragment.findNavController(AccountSelectionFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
--- app/src/main/java/com/example/roleplaybanking/SettingsActivity.java
+++ app/src/main/java/com/example/roleplaybanking/SettingsActivity.java
@@ -1,24 +1,22 @@
 package com.example.roleplaybanking;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account_selection);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
