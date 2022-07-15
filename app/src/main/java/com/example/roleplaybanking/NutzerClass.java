37fc4627161cf56d11e0443f3f9e73f7974d2cfd
Argos-880 <85112274+Argos-880@users.noreply.github.com>
2022-07-15 17:24:45 +0200

    Login und Registrieren
    Login und Registrierungs funktionen in datenbankklasse implementiert

---
+++ app/src/main/java/com/example/roleplaybanking/NutzerClass.java
@@ -0,0 +1,34 @@
+package com.example.roleplaybanking;
+
+public class NutzerClass {
+
+    private String Name;
+    private Number NutzerID;
+    private String NutzerName;
+    private String NutzerPW;
+
+    public NutzerClass(){}
+
+    public NutzerClass(String Name, Number NutzerId, String NutzerName, String NutzerPW){
+        this.Name = Name;
+        this.NutzerID = NutzerId;
+        this.NutzerName= NutzerName;
+        this.NutzerPW = NutzerPW;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Number getNutzerID() {
+        return NutzerID;
+    }
+
+    public String getNutzerName() {
+        return NutzerName;
+    }
+
+    public String getNutzerPW() {
+        return NutzerPW;
+    }
+}
