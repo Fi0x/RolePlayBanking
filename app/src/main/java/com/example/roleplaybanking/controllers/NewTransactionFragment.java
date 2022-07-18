6ce6e4227a1748f1c1bab8febb38f47998a84efa
Fi0x <felpech@aol.com>
2022-07-18 14:04:26 +0200

    Changed recipient spinner to auto-complete text-field

--- app/src/main/java/com/example/roleplaybanking/controllers/NewTransactionFragment.java
+++ app/src/main/java/com/example/roleplaybanking/controllers/NewTransactionFragment.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -12,7 +13,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.NavGraph;
 import androidx.navigation.Navigation;
 
 import com.example.roleplaybanking.R;
@@ -20,6 +20,7 @@ import com.example.roleplaybanking.structures.Account;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class NewTransactionFragment extends Fragment {
+    private String[] recipientList = new String[]{"Hans", "Klaus", "Dieter", "Olaf", "Karl"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_transaction, container, false);
@@ -27,11 +28,14 @@ public class NewTransactionFragment extends Fragment {
         TextView txtBalance = view.findViewById(R.id.txtCurrentBalance);
         txtBalance.setText(String.format("%s %s", Account.currentAccount.balance, Account.currentAccount.currencySign));
 
-        Spinner sp = view.findViewById(R.id.spinRecipientList);
-        //TODO: Add possible recipients
+        //TODO: Add possible recipients to recipient list
 
         Button sendButton = view.findViewById(R.id.btnSend);
         sendButton.setOnClickListener(view1 -> sendTransaction(view));
+
+        AutoCompleteTextView txtAutoComplete = view.findViewById(R.id.txtRecipientAutoComplete);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1, recipientList);
+        txtAutoComplete.setAdapter(adapter);
 
         return view;
     }
