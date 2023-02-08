a826bc8955406008e0135b767aadba1b25d15ee7
Fi0x <felpech@aol.com>
2023-02-08 19:24:00 +0100

    Fixed transaction update not showing up without reload

--- app/src/main/java/com/example/roleplaybanking/controllers/AccountOverviewFragment.java
+++ app/src/main/java/com/example/roleplaybanking/controllers/AccountOverviewFragment.java
@@ -1,13 +1,14 @@
 package com.example.roleplaybanking.controllers;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,7 +28,6 @@ public class AccountOverviewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        System.out.println("CreateView called with transaction count: " + transactions.size());
         View view = inflater.inflate(R.layout.fragment_account_overview, container, false);
 
         TextView balance = view.findViewById(R.id.txt_balance);
@@ -35,22 +35,32 @@ public class AccountOverviewFragment extends Fragment {
         DBc = AccountSelectionActivity.DBc;
 
         DBc.orderTransactions();
-        transactions.clear();
-        transactions.addAll(Account.currentAccount.AccountHistory);
-
-        System.out.println("CreateView updated with transaction count: " + transactions.size());
+        updateTransactionList();
 
         final RecyclerView rvTransactions = (RecyclerView) view.findViewById(R.id.rvTransactionHistory);
-        rvTransactions.setAdapter(new TransactionsAdapter(transactions));
+        TransactionsAdapter adapter = new TransactionsAdapter(transactions);
+        rvTransactions.setAdapter(adapter);
         rvTransactions.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
         AccountOverviewActivity.fab.setVisibility(View.VISIBLE);
         AccountOverviewActivity.isSubFragment = false;
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            updateTransactionList();
+            adapter.notifyDataSetChanged();
+        }, 1000);
 
         return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void updateTransactionList()
+    {
+        transactions.clear();
+        transactions.addAll(Account.currentAccount.AccountHistory);
     }
 }
